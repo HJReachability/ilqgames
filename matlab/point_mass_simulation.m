2c2fd4492f8c377efced4068e1c64b781e0a3b2d
jennifershih <cyshih55@gmail.com>
2018-11-13 13:52:00 -0800

    functional for the point mass now

--- matlab/point_mass_simulation.m
+++ matlab/point_mass_simulation.m
@@ -7,11 +7,11 @@
 
 %% Parameters.
 u_max = 4.0; % Maximum acceleration/deceleration (m/s/s).
-max_velocity_delta = 2.0; % Maximum velocity change (m/s).
+max_velocity_delta = 5.0; % Maximum velocity change (m/s).
 min_dist = 1.0; % Closest vehicles should ever get (m).
 
 %% Initial states (p = position, v = velocity).
-ph0 = [20.0, -4.0];
+ph0 = [60.0, -4.0];
 vh0 = [-10.0, 0.0];
 
 pr0 = [0.0, 0.0];
@@ -24,56 +24,78 @@ vr0 = [10.0, 0.0];
 %% Loop over time and figure out maximal T (time to potential unavoidable
 % collision), and apply corresponding control.
 dt = 0.1;
-max_time = 10.0;
+max_time = 5.0;
 times = 0.0:dt:max_time;
 
 ph = ph0; vh = vh0;
 pr = pr0; vr = vr0;
 
+fgh = figure();
+axh = axes('Parent',fgh); 
+scatter(ph(2), ph(1), 'ro');
+scatter(pr(2), pr(1), 'bo');
+axis([-4 4 0 100]);
+
 for t = times
   % Compute time till states get close in x, y.
-  [max_tx, ty] = compute_max_tx_ty(ph, vh, pr, vr);
-  T = max(max_tx, ty);
+  p_rel = ph - pr; 
+  vr_desired = vr0;
 
-  % Determine robot's ideal speed. This will either be the nominal
-  % speed or it will mean slowing down as much as possible.
-  % TODO! Make this max velocity change relative to last speed not
-  % initial speed. That will help keep things dynamically feasible.
-  vr_desired = vr0
-  if T = max_tx
-    vr_desired(1) = vr_desired(1) - max_velocity_delta;
+  if p_rel(1) >= 0
+      [max_tx, ty] = compute_max_tx_ty(ph, vh, pr, vr, max_velocity_delta, ...
+          min_dist, u_max);
+      T = max(max_tx, ty);
+
+      % Determine robot's ideal speed. This will either be the nominal
+      % speed or it will mean slowing down as much as possible.
+      % TODO! Make this max velocity change relative to last speed not
+      % initial speed. That will help keep things dynamically feasible.
+      if max_tx > ty
+        vr_desired(1) = vr_desired(1) - max_velocity_delta;
+      end
   end
 
   % Update human and robot states.
   ph = ph + vh * dt;
   pr = pr + vr_desired * dt;
 
+  
+  vr_desired
+  vh0
   % Plot/animate this over time.
   % TODO!
+  pause(dt);
+  hold all; 
+  scatter(ph(2), ph(1), 'ro');
+  scatter(pr(2), pr(1), 'bo');
+  
+   
 end
 
 %% Utility function to compute max time to "collision" in x and y.
-function [tx, ty] = compute_max_tx_ty(ph, vh, pr, vr)
+function [tx, ty] = compute_max_tx_ty(ph, vh, pr, vr, max_velocity_delta, ...
+    min_dist, u_max)
   p_rel = ph - pr;
   v_rel = vh - vr;
 
   % Adjust v_rel in x-direction so as to maximize time to collision in x.
-  if v_rel < 0
-    v_rel(1) = v_rel(1) + max_velocity_delta;
-  end
+  v_rel(1) = v_rel(1) + max_velocity_delta;
 
   % Time till states get too close in x is either 0 (if already that close)
   % or something more complicated.
   tx = 0.0;
   if abs(p_rel(1)) > min_dist
     tx = (v_rel(1) / u_max) * sign(p_rel(1)) + ...
-         sqrt(v_rel(1)*v_rel(1) + u_max(abs(p_rel(1)) - min_dist)) / u_max;
+         sqrt(v_rel(1)*v_rel(1) + u_max * (abs(p_rel(1)) - min_dist)) / u_max;
   end
 
   % Same for time to get close in y.
   ty = 0.0;
   if abs(p_rel(2)) > min_dist
     ty = (v_rel(2) / u_max) * sign(p_rel(2)) + ...
-         sqrt(v_rel(2)*v_rel(2) + u_max(abs(p_rel(2)) - min_dist)) / u_max;
+         sqrt(v_rel(2)*v_rel(2) + u_max * (abs(p_rel(2)) - min_dist)) / u_max;
   end
+  
+  tx
+  ty
 end
