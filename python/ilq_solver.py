7fb9421c78b1cd2ef2daec1cbb8107f98cebbbf5
Ellis Ratner <ellis.ratner@gmail.com>
2019-02-21 17:11:47 -0800

    refactored ilq game solver to use the new two-player dynamical system class

--- python/ilq_solver.py
+++ python/ilq_solver.py
@@ -41,7 +41,6 @@ import numpy as np
 from scipy.linalg import block_diag
 import torch
 
-from dynamical_system import DynamicalSystem
 from two_player_dynamical_system import TwoPlayerDynamicalSystem
 from player_cost import PlayerCost
 from semiquadratic_cost import SemiquadraticCost
@@ -54,24 +53,20 @@ from point import Polyline
 from line_segment import LineSegment
 
 class ILQSolver(object):
-    def __init__(self, dynamics1, dynamics2, player1_cost, player2_cost,
-                 x1, x2, P1s, P2s, alpha1s, alpha2s):
+    def __init__(self, dynamics, player1_cost, player2_cost,
+                 x0, P1s, P2s, alpha1s, alpha2s):
         """
         Initialize from dynamics, player costs, current state, and initial
         guesses for control strategies for both players.
 
-        :param dynamics1: dynamical system for player 1
-        :type dynamics1: DynamicalSystem
-        :param dynamics2: dynamical system for player 2
-        :type dynamics2: DynamicalSystem
+        :param dynamics: two-player dynamical system 
+        :type dynamics: TwoPlayerDynamicalSystem
         :param player1_cost: cost function for player 1
         :type player1_cost: PlayerCost
         :param player2_cost: cost function for player 2
         :type player2_cost: PlayerCost
-        :param x1: initial state of player 1
-        :type x1: np.array
-        :param x2: initial state of player 1
-        :type x2: np.array
+        :param x0: initial state 
+        :type x0: np.array
         :param P1s: list of feedback gains for player 1
         :type P1s: [np.array]
         :param P2s: list of feedback gains for player 2
@@ -81,12 +76,10 @@ class ILQSolver(object):
         :param alpha2s: list of constant offsets for player 2
         :type alpha2s: [np.array]
         """
-        self._dynamics1 = dynamics1
-        self._dynamics2 = dynamics2
+        self._dynamics = dynamics
         self._player1_cost = player1_cost
         self._player2_cost = player2_cost
-        self._x1 = x1
-        self._x2 = x2
+        self._x0 = x0
         self._P1s = P1s
         self._P2s = P2s
         self._alpha1s = alpha1s
@@ -105,39 +98,35 @@ class ILQSolver(object):
         """ Run the algorithm for the specified parameters. """
         while not self._is_converged():
             # (1) Compute current operating point.
-            x1s, u1s, x2s, u2s = self._compute_operating_point()
+            xs, u1s, u2s = self._compute_operating_point()
 
             # (2) Linearize about this operating point. Make sure to
             # stack appropriately since we will concatenate state vectors
             # but not control vectors, so that
-            #    ``` x_{k+1}= A_k x_k + B1_k u1_k + B2_k u2_k + c_k ```
+            #    ``` x_{k+1} - xs_k = A_k (x_k - xs_k) + 
+            #          B1_k (u1_k - u1s_k) + B2_k (u2_k - u2s_k) + c_k ```
             As = []
             B1s = []
             B2s = []
             cs = []
             for ii in range(self._horizon):
-                A1, B1, c1 = self._dynamics1.linearize_discrete(
-                    x1s[ii], u1s[ii])
-                A2, B2, c2 = self._dynamics2.linearize_discrete(
-                    x2s[ii], u2s[ii])
+                A, B1, B2, c = self._dynamics.linearize_discrete(
+                    x1s[ii], u1s[ii], u2s[ii])
 
-                As.append(block_diag(A1, A2))
+                As.append(A)
                 B1s.append(B1)
                 B2s.append(B2)
-                cs.append(np.concatenate([c1, c2], axis=0))
+                cs.append(c)
 
             # (3) Quadraticize costs.
             cost1s = []; Q1s = []; l1s = []; R11s = []; R12s = []
             cost2s = []; Q2s = []; l2s = []; R21s = []; R22s = []
             for ii in range(self._horizon):
-                # Concatenate state vectors.
-                x = np.concatenate([x1s[ii], x2s[ii]], axis=0)
-
                 # Quadraticize everything!
                 cost1, l1, Q1, R11, R12 = self._player1_cost.quadraticize(
-                    x, u1s[ii], u2s[ii])
+                    xs[ii], u1s[ii], u2s[ii])
                 cost2, l2, Q2, R21, R22 = self._player2_cost.quadraticize(
-                    x, u1s[ii], u2s[ii])
+                    xs[ii], u1s[ii], u2s[ii])
 
             # (4) Compute feedback Nash equilibrium of the resulting LQ game.
             P1s, P2s, alpha1s, alpha2s = solve_lq_game(
@@ -156,17 +145,16 @@ class ILQSolver(object):
         """
         Compute current operating point by propagating through dynamics.
 
-        :return: states and controls for both players (x1s, u1s, x2s, u2s)
-        :rtype: [np.array], [np.array], [np.array], [np.array]
+        :return: state and controls for both players (xs, u1s, u2s)
+        :rtype: [np.array], [np.array], [np.array]
         """
-        x1s = [self._x1]
+        xs = [self._x0]
         u1s = []
-        x2s = [self._x2]
         u2s = []
 
         for k in range(self._horizon):
-            u1 = -self._P1s[k] @ x1s[k] - self._alpha1s[k]
-            u2 = -self._P2s[k] @ x2s[k] - self._alpha2s[k]
+            u1 = -self._P1s[k] @ xs[k] - self._alpha1s[k]
+            u2 = -self._P2s[k] @ xs[k] - self._alpha2s[k]
 
             u1s.append(u1)
             u2s.append(u2)
@@ -174,13 +162,11 @@ class ILQSolver(object):
             if k == self._horizon - 1:
                 break
 
-            x1 = self._dynamics1.integrate(x1s[k], u1)
-            x2 = self._dynamics2.integrate(x2s[k], u2)
+            x = self._dynamics.integrate(xs[k], u1, u2)
 
-            x1s.append(x1)
-            x2s.append(x2)
+            xs.append(x)
 
-        return x1s, u1s, x2s, u2s
+        return xs, u1s, u2s
 
     def _linesearch(self):
         """ Linesearch for both players separately. """
@@ -199,13 +185,10 @@ class ILQSolver(object):
         # within this tolerance in the Euclidean norm then we've converged.
         TOLERANCE = 1e-4
         for ii in range(self._horizon):
-            last_x1 = self._last_operating_point[0][ii]
-            current_x1 = self._current_operating_point[0][ii]
-            last_x2 = self._last_operating_point[2][ii]
-            current_x2 = self._current_operating_point[2][ii]
+            last_x = self._last_operating_point[0][ii]
+            current_x = self._current_operating_point[0][ii]
 
-            if np.linalg.norm(last_x1 - current_x1) > TOLERANCE or \
-               np.linalg.norm(last_x2 - current_x2) > TOLERANCE:
+            if np.linalg.norm(last_x - current_x) > TOLERANCE:
                 return False
 
         return True
