e5fa553165177cbfcbe6757f4c5bb9c033b40e16
David Fridovich-Keil <dfk@berkeley.edu>
2019-03-06 20:41:57 -0800

    finished refactoring ilq_solver

--- python/ilq_solver.py
+++ python/ilq_solver.py
@@ -80,6 +80,8 @@ class ILQSolver(object):
         self._alphas = alphas
         self._u_constraints = u_constraints
         self._horizon = len(P1s)
+        self._num_players = len(player_costs)
+        self._u_dims = [Pis[0].shape[0] for Pis in Ps]
 
         # Current and previous operating points (states/controls) for use
         # in checking convergence.
@@ -99,13 +101,16 @@ class ILQSolver(object):
 
         while not self._is_converged():
             # (1) Compute current operating point and update last one.
-            xs, u1s, u2s, cost1s, cost2s = self._compute_operating_point()
+            xs, us, costs = self._compute_operating_point()
             self._last_operating_point = self._current_operating_point
-            self._current_operating_point = (xs, u1s, u2s, cost1s, cost2s)
+            self._current_operating_point = (xs, us, costs)
 
             if self._visualizer is not None:
-                self._visualizer.add_trajectory(iteration, {
-                    "xs" : xs, "u1s" : u1s, "u2s" : u2s})
+                traj = {"xs" : xs}
+                for ii in range(self._num_players):
+                    traj["u%ds" % ii] = us[ii]
+
+                self._visualizer.add_trajectory(iteration, traj)
                 plt.clf()
                 self._visualizer.plot()
                 plt.pause(0.1)
@@ -114,63 +119,47 @@ class ILQSolver(object):
             # stack appropriately since we will concatenate state vectors
             # but not control vectors, so that
             #    ``` x_{k+1} - xs_k = A_k (x_k - xs_k) +
-            #          B1_k (u1_k - u1s_k) + B2_k (u2_k - u2s_k) ```
+            #          sum_i Bi_k (ui_k - uis_k) ```
             As = []
-            B1s = []
-            B2s = []
-            for ii in range(self._horizon):
-                A, B1, B2  = self._dynamics.linearize_discrete(
-                    xs[ii], u1s[ii], u2s[ii])
+            Bs = []
+            for k in range(self._horizon):
+                A, B = self._dynamics.linearize_discrete(
+                    xs[k], [uis[k] for uis in us])
                 As.append(A)
-                B1s.append(B1)
-                B2s.append(B2)
+                Bs.append(B)
 
             # (3) Quadraticize costs.
-            Q1s = []; l1s = []; R11s = []; R12s = []
-            Q2s = []; l2s = []; R21s = []; R22s = []
-            for ii in range(self._horizon):
-                # Quadraticize everything!
-                _, l1, Q1, R11, R12 = self._player1_cost.quadraticize(
-                    xs[ii], u1s[ii], u2s[ii])
-                _, l2, Q2, R21, R22 = self._player2_cost.quadraticize(
-                    xs[ii], u1s[ii], u2s[ii])
+            Qs = [[] for ii in range(self._num_players)]
+            ls = [[] for ii in range(self._num_players)]
+            Rs = [[] for ii in range(self._num_players)]
+            for ii in range(self._num_players):
+                for k in range(self._horizon):
+                    _, l, Q, R = self._player_costs[ii].quadraticize(
+                        xs[k], [uis[k] for uis in us])
 
-                Q1s.append(Q1)
-                l1s.append(l1)
-                R11s.append(R11)
-                R12s.append(R12)
-
-                Q2s.append(Q2)
-                l2s.append(l2)
-                R21s.append(R21)
-                R22s.append(R22)
+                    Qs[ii].append(Q)
+                    ls[ii].append(l)
+                    Rs[ii].append(R)
 
             # (4) Compute feedback Nash equilibrium of the resulting LQ game.
-            P1s, P2s, alpha1s, alpha2s = solve_lq_game(
-                As, B1s, B2s, cs, Q1s, Q2s, l1s, l2s, R11s, R12s, R21s, R22s)
+            Ps, alphas = solve_lq_game(As, Bs, Qs, ls, Rs)
 
             # Accumulate total costs for both players.
-            total_cost1 = sum(cost1s).item()
-            total_cost2 = sum(cost2s).item()
-            print("Total cost for player 1 vs. 2: %f vs. %f." %
-                  (total_cost1, total_cost2))
+            total_costs = [sum(costis).item() for costis in costs]
+            print("Total cost for all players: ", total_costs)
 
             # Log everything.
             if self._logger is not None:
                 self._logger.log("xs", xs)
-                self._logger.log("u1s", u1s)
-                self._logger.log("u2s", u2s)
-                self._logger.log("total_cost1", total_cost1)
-                self._logger.log("total_cost2", total_cost2)
+                self._logger.log("us", us)
+                self._logger.log("total_costs", total_costs)
                 self._logger.dump()
 
             # Update the member variables.
-            self._P1s = P1s
-            self._P2s = P2s
-            self._alpha1s = alpha1s
-            self._alpha2s = alpha2s
+            self._Ps = Ps
+            self._alphas = alphas
 
-            # (5) Linesearch separately for both players.
+            # (5) Linesearch.
             self._linesearch()
             iteration += 1
 
@@ -178,59 +167,47 @@ class ILQSolver(object):
         """
         Compute current operating point by propagating through dynamics.
 
-        :return: state and controls for both players (xs, u1s, u2s)
-        :rtype: [np.array], [np.array], [np.array]
+        :return: states, controls for all players (list of lists), and
+            costs over time (list of lists), i.e. (xs, us, costs)
+        :rtype: [np.array], [[np.array]], [[torch.Tensor(1, 1)]]
         """
         xs = [self._x0]
-        u1s = []
-        u2s = []
-        cost1s = []
-        cost2s = []
+        us = [[] for ii in range(self._num_players)]
+        costs = [[] for ii in range(self._num_players)]
 
         for k in range(self._horizon):
             if self._current_operating_point is not None:
                 current_x = self._current_operating_point[0][k]
-                current_u1 = self._current_operating_point[1][k]
-                current_u2 = self._current_operating_point[2][k]
+                current_u = self._current_operating_point[1][k]
             else:
                 current_x = np.zeros((self._dynamics._x_dim, 1))
-                current_u1 = np.zeros((self._dynamics._u1_dim, 1))
-                current_u2 = np.zeros((self._dynamics._u2_dim, 1))
+                current_u = [np.zeros((ui_dim, 1)) for ui_dim in self._u_dims]
 
-            u1 = current_u1 - self._P1s[k] @ (
-                xs[k] - current_x) - self._alpha_scaling * self._alpha1s[k]
-            u2 = current_u2 - self._P2s[k] @ (
-                xs[k] - current_x) - self._alpha_scaling * self._alpha2s[k]
+            feedback = lambda x, u_ref, x_ref, P, alpha : \
+                       u_ref - P @ (x - x_ref) - self._alpha_scaling * alpha
+            u = [feedback(xs[k], current_u[ii], current_x, self._alphas[ii][k])
+                 for ii in range(self._num_players)]
 
             # Clip u1 and u2.
-#            u1 = self._u1_constraint.clip(u1)
-#            u2 = self._u2_constraint.clip(u2)
+#            for ii in range(self._num_players):
+#                u[ii] = self._u_constraints[ii].clip(u[ii])
 
-            u1s.append(u1)
-            u2s.append(u2)
-
-            cost1s.append(self._player1_cost(torch.as_tensor(xs[k].copy()),
-                                             torch.as_tensor(u1.copy()),
-                                             torch.as_tensor(u2.copy())))
-            cost2s.append(self._player2_cost(torch.as_tensor(xs[k].copy()),
-                                             torch.as_tensor(u1.copy()),
-                                             torch.as_tensor(u2.copy())))
+            for ii in range(self._num_players):
+                us[ii].append(u[ii])
+                costs[ii].append(self._player_costs[ii](
+                    torch.as_tensor(xs[k].copy()),
+                    [torch.as_tensor(ui) for ui in u]))
 
             if k == self._horizon - 1:
                 break
 
-            x = self._dynamics.integrate(xs[k], u1, u2)
-            xs.append(x)
+            xs.append(self._dynamics.integrate(xs[k], u))
 
-        return xs, u1s, u2s, cost1s, cost2s
+        return xs, us, costs
 
     def _linesearch(self):
         """ Linesearch for both players separately. """
         pass
-        # HACK: This is simple, need an actual linesearch.
-#        for ii in range(self._horizon):
-#            self._alpha1s[ii] *= self._alpha_scaling
-#            self._alpha2s[ii] *= self._alpha_scaling
 
     def _is_converged(self):
         """ Check if the last two operating points are close enough. """
