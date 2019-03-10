38363b706b319ff2fed83a62968241472ec12f12
David Fridovich-Keil <dfk@berkeley.edu>
2019-03-09 18:50:34 -0800

    adding proximity cost for product state

--- python/product_state_proximity_cost.py
+++ python/product_state_proximity_cost.py
@@ -33,58 +33,60 @@ Author(s): David Fridovich-Keil ( dfk@eecs.berkeley.edu )
 """
 ################################################################################
 #
-# Obstacle cost, derived from Cost base class. Implements a cost function that
-# depends only on state and penalizes min(0, dist - max_distance)^2.
+# Proximity cost for state spaces that are Cartesian products of individual
+# systems' state spaces. Penalizes
+#      ``` sum_{i \ne j} min(distance(i, j) - max_distance, 0)^2 ```
+# for all players i, j.
 #
 ################################################################################
 
 import torch
-import numpy as np
-import matplotlib.pyplot as plt
 
 from cost import Cost
-from point import Point
 
-class ObstacleCost(Cost):
-    def __init__(self, position_indices, point, max_distance, name=""):
+class ProductStateProximityCost(Cost):
+    def __init__(self, position_indices, max_distance, name=""):
         """
-        Initialize with dimension to add cost to and a max distance beyond
-        which we impose no additional cost.
+        Initialize with dimension to add cost to and threshold BELOW which
+        to impose quadratic cost.
 
-        :param position_indices: indices of input corresponding to (x, y)
-        :type position_indices: (uint, uint)
-        :param point: center of the obstacle from which to compute distance
-        :type point: Point
+        :param position_indices: list of index tuples corresponding to (x, y)
+        :type position_indices: [(uint, uint)]
         :param max_distance: maximum value of distance to penalize
-        :type threshold: float
+        :type max_distance: float
         """
-        self._x_index, self._y_index = position_indices
-        self._point = point
+        self._position_indices = position_indices
         self._max_distance = max_distance
-        super(ObstacleCost, self).__init__(name)
+        self._num_players = len(position_indices)
+        super(ProductStateProximityCost, self).__init__(name)
 
     def __call__(self, x):
         """
-        Evaluate this cost function on the given input state.
+        Evaluate this cost function on the given state.
+        NOTE: `x` should be a PyTorch tensor with `requires_grad` set `True`.
         NOTE: `x` should be a column vector.
 
-        :param x: concatenated state of the two systems
+        :param x: concatenated state vector of all systems
         :type x: torch.Tensor
         :return: scalar value of cost
         :rtype: torch.Tensor
         """
-        # Compute relative distance.
-        dx = x[self._x_index, 0] - self._point.x
-        dy = x[self._y_index, 0] - self._point.y
-        relative_distance = torch.sqrt(dx*dx + dy*dy)
+        total_cost = torch.zeros(1, 1, requires_grad=True).double()
 
-        return min(relative_distance - self._max_distance, torch.zeros(
-            1, 1, requires_grad=True).double())**2
+        for ii in range(self._num_players):
+            xi_idx, yi_idx = self._position_indices[ii]
 
-    def render(self, ax=None):
-        """ Render this obstacle on the given axes. """
-        circle = plt.Circle(
-            (self._point.x, self._point.y), self._max_distance,
-            color="r", fill=True, alpha=0.75)
-        ax.add_artist(circle)
-        ax.text(self._point.x - 1.25, self._point.y - 1.25, "obs", fontsize=8)
+            for jj in range(self._num_players):
+                if ii == jj:
+                    continue
+
+                # Compute relative distance.
+                xj_idx, yj_idx = self._position_indices[jj]
+                dx = x[xi_idx, 0] - x[xj_idx, 0]
+                dy = x[yi_idx, 0] - x[yj_idx, 0]
+                relative_distance = torch.sqrt(dx*dx + dy*dy)
+
+                total_cost += min(
+                    relative_distance - self._max_distance, 0.0)**2
+
+        return total_cost
