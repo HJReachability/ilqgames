b416f39a6a52ac304430fa7d88d768a97646c277
David Fridovich-Keil <dfk@berkeley.edu>
2019-04-17 21:28:21 -0700

    adding glfw

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -16,6 +16,22 @@ include("cmake/External/glog.cmake")
 include_directories(SYSTEM ${GLOG_INCLUDE_DIRS})
 list(APPEND ilqgames_LIBRARIES ${GLOG_LIBRARIES})
 
+# Find OpenGL.
+find_package( OpenGL REQUIRED )
+include_directories(SYSTEM ${OPENGL_INCLUDE_DIRS})
+list(APPEND ilqgames_LIBRARIES ${OPENGL_LIBRARIES})
+
+# Find GLUT.
+find_package( GLUT REQUIRED )
+include_directories(SYSTEM ${GLUT_INCLUDE_DIRS})
+list(APPEND ilqgames_LIBRARIES ${GLUT_LIBRARIES})
+
+# Find GLFW.
+#find_package(PkgConfig REQUIRED)
+#pkg_search_module(GLFW3 REQUIRED glfw)
+#include_directories(${GLFW_INCLUDE_DIRS})
+#list(APPEND ilqgames_LIBRARIES ${GLFW_STATIC_LIBRARIES} ${GLFW_LIBRARIES})
+
 # Find Boost functional module.
 #find_package( Boost REQUIRED )
 #include_directories(SYSTEM ${BOOST_INCLUDE_DIRS})
