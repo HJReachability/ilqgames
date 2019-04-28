96f4e3360f26253479041b128abc4fac681913a7
David Fridovich-Keil <dfk@berkeley.edu>
2019-04-28 16:32:57 -0400

    adding semiquadratic polyline2 cost

--- src/ilq_solver.cpp
+++ src/ilq_solver.cpp
@@ -208,7 +208,7 @@ bool ILQSolver::ModifyLQStrategies(
 
   // As a simple starting point, just scale all the 'alphas' in the strategy to
   // a fraction of their original value.
-  constexpr float kAlphaScalingFactor = 0.2;
+  constexpr float kAlphaScalingFactor = 0.05;
   for (auto& strategy : *strategies) {
     for (auto& alpha : strategy.alphas) alpha *= kAlphaScalingFactor;
   }
