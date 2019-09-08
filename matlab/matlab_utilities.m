2f6ac767a8015f1981cecdbed9ed8ec633ea30f1
MrRubyRed <vrubies@berkeley.edu>
2019-09-07 17:54:29 -0700

    Added non-linear overtaking example. Linear one not working.

--- matlab/matlab_utilities.m
+++ matlab/matlab_utilities.m
@@ -1,7 +1,10 @@
-test_region_size_vs_alpha_scaling('three_player_flat_intersection');
-% plot_convergence('Thu_Sep__5_11_07_29_2019');
+% Test region size vs. alpha scaling
+test_region_size_vs_alpha_scaling('three_player_intersection',5,5);
+
+% Plotting convergence of two different experiments
+% plot_convergence('Sat_Sep__7_15_37_49_2019');
 % hold on
-% plot_convergence('Thu_Sep__5_11_09_55_2019');
+% plot_convergence('Sat_Sep__7_15_38_38_2019');
 
 % Norm of the difference between trajectories.
 function y = trajectory_d(traj1,traj2)
@@ -24,14 +27,16 @@ function plot_trajectory(trajectory)
     x = trajectory(:,13);
     y = trajectory(:,14);
     plot(x,y,'g');
+    axis equal
 end
 
 % Plot trajectory for a certain experiment
-function plot_experiment_traj(experiment_folder)
+function runtime = plot_experiment_traj(experiment_folder)
     cd(experiment_folder);
     dirs = struct2cell(dir);
     cd(dirs{1,3});
     traj = load("xs.txt");
+    runtime = load("runtimes.txt");
     plot_trajectory(traj);
     cd('..');
     cd('..');
@@ -39,6 +44,7 @@ end
 
 % This script plots ||traj_final - traj_iter||
 function plot_convergence(experiment_name)
+    cd('../logs')
     cd(experiment_name);
     dirs = dir;
     % Record final trajectory for comparison
@@ -55,33 +61,57 @@ function plot_convergence(experiment_name)
     end
     plot(convergence_value);
     cd('..');
+    cd('../matlab')
 end
 
-function test_region_size_vs_alpha_scaling(exec1)
+function y=check_if_folder_exists(folder_name)
+    % Get experiment folder names
+    cd('../logs')
+    dirs = dir;
+    y = false;
+    for i=1:length(dirs)
+        if strcmp(folder_name,dirs(i).name)
+            y=true;
+            break;
+        end
+    end
+    cd('../matlab');
+end
+
+function test_region_size_vs_alpha_scaling(exec1,n,m)
+    prefix = "../logs";
     folder = "../bin/";
     flag1 = " -trust_region_size=";
     flag2 = " -initial_alpha_scaling=";
     experiment_arg = "-experiment_name=";
-    n = 3;
-    m = 3;
-    range_for_tregion = linspace(10,100,n);
-    range_for_alphasc = linspace(0.1,1.0,m);
+    
+    range_for_tregion = linspace(1.0,20.0,n);
+    range_for_alphasc = linspace(0.1,0.75,m);
     j = 1;
     for tr_size=range_for_tregion
        for alpha_scaling=range_for_alphasc
+           
+           % Convert parameters loop parameters to strings for names
            string_alpha_sc = string(num2str(alpha_scaling));
            string_tr_size  = string(num2str(tr_size));
            tmp =exec1+"_trsize_" + num2str(tr_size) + ...
                                                "_alphasc_" + num2str(alpha_scaling);
            exp_folder_name = "'"+tmp + "'";
            experiment_name = experiment_arg + exp_folder_name;
-           instruction = folder + exec1 + flag1 + string_tr_size + ...
-                         flag2 + string_alpha_sc + " -viz=false -save=true -last_traj=true " + ...
-                         experiment_name;
-           system(char(instruction));
+           
+           exists = check_if_folder_exists(char(tmp));
+           if ~exists
+               % Stitch together the command for the executable.
+               instruction = folder + exec1 + flag1 + string_tr_size + ...
+                             flag2 + string_alpha_sc + " -viz=false -save=true -last_traj=true " + ...
+                             experiment_name;
+               system(char(instruction));
+           end
            subplot(n,m,j);
-           plot_experiment_traj(char(tmp));
+           runtime = plot_experiment_traj(char(prefix+"/"+tmp));
+           title("as=" + string_alpha_sc + ",tr=" + string_tr_size + "(" +num2str(runtime)+ ")");
            j = j+1;
        end
     end
+    cd('../matlab')
 end
