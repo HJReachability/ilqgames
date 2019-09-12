a0355c47d4eacea8db2881e55544ebd48393142a
David Fridovich-Keil <dfk@berkeley.edu>
2019-09-11 18:47:18 -0700

    adding roundabout non flat example. probably could use some more tuning to make it more exciting

--- include/ilqgames/examples/roundabout_merging_example.h
+++ include/ilqgames/examples/roundabout_merging_example.h
@@ -36,53 +36,29 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-// Line segment in 2D.
+// Roundabout merging example.
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-#ifndef ILQGAMES_GEOMETRY_LINE_SEGMENT2_H
-#define ILQGAMES_GEOMETRY_LINE_SEGMENT2_H
+#ifndef ILQGAMES_EXAMPLE_ROUNDABOUT_MERGING_EXAMPLE_H
+#define ILQGAMES_EXAMPLE_ROUNDABOUT_MERGING_EXAMPLE_H
 
-#include <ilqgames/utils/types.h>
-
-#include <glog/logging.h>
+#include <ilqgames/solver/problem.h>
+#include <ilqgames/solver/solver_params.h>
+#include <ilqgames/solver/top_down_renderable_problem.h>
 
 namespace ilqgames {
 
-class LineSegment2 {
+class RoundaboutMergingExample : public TopDownRenderableProblem {
  public:
-  ~LineSegment2() {}
-  LineSegment2(const Point2& point1, const Point2& point2)
-      : p1_(point1),
-        p2_(point2),
-        length_((point1 - point2).norm()),
-        unit_direction_((point2 - point1) / length_) {
-    CHECK_GT(length_, constants::kSmallNumber);
-  }
+  ~RoundaboutMergingExample() {}
+  RoundaboutMergingExample(const SolverParams& params);
 
-  // Accessors.
-  float Length() const { return length_; }
-  const Point2& FirstPoint() const { return p1_; }
-  const Point2& SecondPoint() const { return p2_; }
-  const Point2& UnitDirection() const { return unit_direction_; }
-  float Heading() const {
-    return std::atan2(UnitDirection().y(), UnitDirection().x());
-  }
-
-  // Find closest point on this line segment to a given point (and optionally
-  // the signed squared distance, where right is positive, and whether or not
-  // the closest point is an endpoint).
-  Point2 ClosestPoint(const Point2& query, bool* is_endpoint = nullptr,
-                      float* signed_squared_distance = nullptr) const;
-
-  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-
- private:
-  Point2 p1_;
-  Point2 p2_;
-  float length_;
-  Point2 unit_direction_;
-};  // struct LineSegment2
+  // Unpack x, y, heading (for each player, potentially) from a given state.
+  std::vector<float> Xs(const VectorXf& x) const;
+  std::vector<float> Ys(const VectorXf& x) const;
+  std::vector<float> Thetas(const VectorXf& x) const;
+};  // class RoundaboutMergingExample
 
 }  // namespace ilqgames
 
