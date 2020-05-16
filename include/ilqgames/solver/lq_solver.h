786106ad310807e16309ac1488564b8ec37a48ea
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-16 03:36:23 +0000

    Deploy HJReachability/ilqgames to github.com/HJReachability/ilqgames.git:gh-pages

--- include/ilqgames/solver/lq_solver.h
+++ include/ilqgames/solver/lq_solver.h
@@ -50,6 +50,7 @@
 #include <ilqgames/utils/strategy.h>
 
 #include <vector>
+#include <glog/logging.h>
 
 namespace ilqgames {
 
@@ -60,14 +61,21 @@ class LQSolver {
   // Solve underlying LQ game to a Nash equilibrium. This will differ in derived
   // classes depending on the information structure of the game.
   virtual std::vector<Strategy> Solve(
-      const MultiPlayerIntegrableSystem& dynamics,
       const std::vector<LinearDynamicsApproximation>& linearization,
       const std::vector<std::vector<QuadraticCostApproximation>>&
           quadraticization,
       const VectorXf& x0) = 0;
 
  protected:
-  LQSolver() {}
+  LQSolver(const std::shared_ptr<const MultiPlayerIntegrableSystem>& dynamics,
+           size_t num_time_steps)
+      : dynamics_(dynamics), num_time_steps_(num_time_steps) {
+    CHECK_NOTNULL(dynamics.get());
+  }
+
+  // Dynamics and number of time steps.
+  const std::shared_ptr<const MultiPlayerIntegrableSystem> dynamics_;
+  const size_t num_time_steps_;
 };  // class LQSolver
 
 }  // namespace ilqgames
