6233e12daad11f20259fb9209872744d30f8a9df
chihyuanchiu <chihyuan_chiu@berkeley.edu>
2021-09-08 16:44:30 -0700

    Fixing skeleton_example main

--- exec/skeleton_example/main.cpp
+++ exec/skeleton_example/main.cpp
@@ -75,7 +75,7 @@ DEFINE_double(control_regularization, 1.0, "Control regularization.");
 
 // Linesearch parameters.
 DEFINE_bool(linesearch, true, "Should the solver linesearch?");
-DEFINE_double(initial_alpha_scaling, 0.1, "Initial step size in linesearch.");
+DEFINE_double(initial_alpha_scaling, 0.25, "Initial step size in linesearch.");
 DEFINE_double(convergence_tolerance, 0.01, "KKT squared error tolerance.");
 DEFINE_double(expected_decrease, 0.1, "KKT sq err expected decrease per iter.");
 
